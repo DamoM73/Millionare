e44f6eda9afebfdcb6d7e8365d03af8ba7f310b4
DamoM <damomurtagh@gmail.com>
2018-08-01 12:42:42 +1000

    First Stage

--- main.py
+++ main.py
@@ -7,22 +7,35 @@ Detail both the user inputs and the program outputs
 # import libaries
 from tkinter import *
 
-#---- Constants
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Title"
+#---- CONSTANTS
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Who wants to be a Millionaire?"
 
-#---- Functions executed when buttons are pressed ----
+#---- CLASSES
 
+#---- FUNCTIONS ----
 
-#---- Create a fixed size window ----
+#-- General Functions --
+
+#-- Functions executed when buttons are pressed --
+
+#---- DRAW GUI
+#-- Create a fixed size window ----
 root = Tk()
 root.geometry(str(SCREEN_WIDTH) + "x" + str(SCREEN_HEIGHT))
-root.Title = SCREEN_TITLE
-root.wm_resizable(False, False)             # to resize, change to True (Width, Height)
-root.configure(background = "Steel blue")   # refer to chart for available colour names
+root.title(SCREEN_TITLE)
+root.wm_resizable(False, False)
+root.configure(background = "Steel blue")
 
-#---- Create frames for different sections
+#-- Create frames for different sections
+# copy the code below for each section in your GUI
 
-frame_one = Frame(root)
-frame_one.grid(row = 0, column = 0, columnspan = 1, padx = 25, pady = 25)
+frame_title = Frame(root)
+frame_title.grid(row = 0, column = 0, columnspan = 1, padx = 25, pady = 25)
+
+#-- Place objects
+
+
+#---- MAIN LOOP
+root.mainloop()
